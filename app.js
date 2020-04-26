0d37d53e54bc326221bbb6f20c95270d06a4c7b0
sanket-1999 <60914794+sanket-1999@users.noreply.github.com>
2020-04-26 13:26:19 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,44 @@
+
+import "babel-polyfill";
+
+import { TestConst } from './components/const.js';
+import { Scoping } from './components/scoping.js';
+import { Order , printorder , getprice} from './components/order.js';
+import { Array } from './components/array.js';
+import { add } from './components/add.js';
+import { userfriends } from './components/userfriends.js';
+import { printCapitalNames } from './components/printCapitalNames.js';
+import { template } from './components/tempelateliteral.js';
+import { destruct } from './components/destruct.js';
+import { deepmatch } from "./components/deepmatch.js";
+import { total  } from './components/bank.js';
+
+
+
+// import { fibonacci } from './components/fibonaci.js';
+import { arm  }  from './components/arm.js';
+/// import { armm  } from './components/armm.js';
+
+import { promise  } from './components/promise.js';
+
+//TestConst();
+//Scoping();
+//Order();
+//printorder();
+//getprice();
+//Array();
+//add();
+//userfriends();
+//printCapitalNames();
+//template();
+//destruct()
+//deepmatch();
+//total();
+
+//fibonacci();
+//arm();
+
+//armm();
+
+
+promise();
